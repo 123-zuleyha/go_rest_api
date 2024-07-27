f4719ee658084d7d659f34dadd057e9ecfc922a1
123-zuleyha <97296931+123-zuleyha@users.noreply.github.com>
2024-07-27 17:12:49 +0300

    Update main.go

--- go_rest_api/main.go
+++ go_rest_api/main.go
@@ -1,12 +1,12 @@
 package main 
 import (
-	// Veritabanı bağlantı işlemleri için kullanılan paket	
-     "github.com/123-zuleyha/go_rest_api/database" 
-     "github.com/123-zuleyha/go_rest_api/router" // Rota ayarları için kullanılan paket
- "github.com/gofiber/fiber/v2" // Fiber web framework'ü için kullanılan paket
- "github.com/gofiber/fiber/v2/middleware/cors" // CORS (Cross-Origin Resource Sharing) middleware'i için kullanılan paket
- "github.com/gofiber/fiber/v2/middleware/logger" // Logger middleware'i için kullanılan paket
- _ "github.com/lib/pq" // PostgreSQL sürücüsü, sadece yan etkileri için içe aktarılıyor
+	
+"github.com/123-zuleyha/go_rest_api/database" 
+ "github.com/123-zuleyha/go_rest_api/router" 
+ "github.com/gofiber/fiber/v2" 
+ "github.com/gofiber/fiber/v2/middleware/cors" 
+ "github.com/gofiber/fiber/v2/middleware/logger"
+ "github.com/lib/pq" 
 
 )
 
